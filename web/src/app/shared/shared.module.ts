c2c3e1639fbdc773186be51a82b0815819cecf2e
martins86 <martins86@gmail.com>
2020-07-02 20:14:23 +0000

    importando mat

--- web/src/app/shared/shared.module.ts
+++ web/src/app/shared/shared.module.ts
@@ -2,24 +2,35 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
 import { HeaderComponent } from './../shared/components/header/header.component';
 import { SidebarComponent } from './../shared/components/sidebar/sidebar.component';
 import { FooterComponent } from './../shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-  HeaderComponent,
-  SidebarComponent,
-  FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
   ],
   exports: [
-  HeaderComponent,
-  SidebarComponent,
-  FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
   ]
 })
 export class SharedModule { }
