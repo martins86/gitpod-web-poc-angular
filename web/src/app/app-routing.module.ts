b2cbd521c3adbd2c9e8bd24fe2b291a3b0c4ea31
martins86 <martins86@gmail.com>
2020-07-02 18:04:30 +0000

    iniciando rotas

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DefaultComponent } from './layout/default/default.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
