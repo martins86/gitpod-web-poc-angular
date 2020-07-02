5a1f4b970e086d9e934176d24c36b119207104b9
martins86 <martins86@gmail.com>
2020-07-02 15:12:03 +0000

    ng g c shared/components/sidebar/

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HeaderComponent } from './shared/components/header/header.component';
     LoginComponent,
     HomeComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
