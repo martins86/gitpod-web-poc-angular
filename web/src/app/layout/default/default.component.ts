96889cb1f910687df1d879eebd2a349d967d23b4
martins86 <martins86@gmail.com>
2020-07-02 21:13:44 +0000

    adicionando função abrir/fechar sidebar

--- web/src/app/layout/default/default.component.ts
+++ web/src/app/layout/default/default.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultComponent implements OnInit {
 
+  sideBarOpen = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //TODO: test
+  sideBarToggle() {
+    this.sideBarOpen = !this.sideBarOpen;
   }
 
 }
