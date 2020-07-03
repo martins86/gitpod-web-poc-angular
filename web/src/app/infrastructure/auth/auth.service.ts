d3770c26eb62d6aea1c82909367b410cb67c8f70
martins86 <martins86@gmail.com>
2020-07-03 20:13:46 +0000

    implementando auth service

--- web/src/app/infrastructure/auth/auth.service.ts
+++ web/src/app/infrastructure/auth/auth.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { environment } from './../../../environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  //TODO: teste
+  login(email, password): Observable<any> {
+    const loginUrl = environment.urlApi + '/signin';
+    const headers = { 'Content-Type': 'application/json' };
+    const body = { email, password };
+
+    return this.http
+      .post<any>(loginUrl, body, { headers })
+      .pipe(catchError(this.handleError));
+  }
+
+  //TODO: teste
+  setOnStorage(authToken) {
+    sessionStorage.setItem('user-token', authToken);
+  }
+
+  //TODO: teste
+  clearStorage() {
+    sessionStorage.clear();
+    sessionStorage.removeItem('user-token');
+  }
+
+  //TODO: teste
+  get userIsLogged() {
+    const authToken = sessionStorage.getItem('user-token');
+    return authToken !== null && authToken !== undefined;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
 }
