c5ac435f7e9cfdce00937659ed3224720881615e
Thiago Martins de Freitas <thiagodf@ciandt.com>
2020-07-06 18:49:07 +0000

    implementando o logout

--- web/src/app/shared/components/header/header.component.ts
+++ web/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { AuthService } from './../../../infrastructure/auth/auth.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,13 +14,19 @@ export class HeaderComponent implements OnInit {
   @Output()
   toggleSideBarForMe: EventEmitter<any> = new EventEmitter();
 
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
   //TODO: test
   toggleSideBar() {
     this.toggleSideBarForMe.emit();
+  }
+
+  //TODO: test
+  logout() {
+    this.authService.clearStorage();
+    this.router.navigate(['/login']);
   }
 
 }
